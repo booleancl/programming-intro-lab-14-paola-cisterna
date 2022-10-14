4af918853192eff076041159e987b9ad1357aa1a
aylinsanmartin <a.sanlazo@gmail.com>
2022-10-14 10:59:59 -0300

    reforsamiento

--- migrations/4_seedData.sql
+++ migrations/4_seedData.sql
@@ -1,1 +1,5 @@
+\c blog
 
+\COPY users(email) FROM 'C:/Users/Acer/intro_dev/programming-intro-lab-14-paola-cisterna/data/users.csv' csv HEADER;
+\COPY posts FROM 'C:/Users/Acer/intro_dev/programming-intro-lab-14-paola-cisterna/data/posts.csv' csv HEADER;
+\COPY comments FROM 'C:/Users/Acer/intro_dev/programming-intro-lab-14-paola-cisterna/data/comentarios.csv' csv HEADER;
