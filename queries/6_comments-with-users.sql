255ed68fc661e8d9e3dd41f7eb681f1694f4b244
aylinsanmartin <a.sanlazo@gmail.com>
2022-10-17 11:05:35 -0300

    blog

--- queries/6_comments-with-users.sql
+++ queries/6_comments-with-users.sql
@@ -1,7 +1,5 @@
-/*
-Seleccionar el correo, id del usuario y el contenido de los comentarios
-*/
+\c blog
+
 SELECT email, users.id, content
 FROM comments
-INNER JOIN users ON users.id = comments.user_id 
-WHERE users.email != 'usuario06@hotmail.com'
+JOIN users ON users.id = comments.user_id 
--- queries/7_lazy-user.sql
+++ queries/7_lazy-user.sql
@@ -1,4 +1,10 @@
 /*
 Seleccionar el id y el correo del usuario que no tenga ningún post
 */
+\c blog
+
+SELECT user.id,email, title
+FROM Users
+JOIN posts ON user_id = posts.user_id;
+WHERE posts.user_id IS NULL;
 
